fb715151dd7e9900de743335b5f5dff0842d354a
Fabio Furlan <furlan.fabio@hotmail.com>
2022-11-18 00:39:36 -0300

    exercicio

--- Exercicio/src/projeto/Name.java
+++ Exercicio/src/projeto/Name.java
@@ -8,26 +8,45 @@ public class Name {
 		// ATRIBUINDO NOME
 		 
 		 	String nome = "fabio";
-		 	System.out.println(nome);
+		 	System.out.println("O nome para comparação: " + nome);
 		 
+		 // TAMANHO DA STRING
+			 System.out.println( "A quantidade de caracter em " + nome + ": " +  nome.length  ( ));
+			 
+			
+				// verificando se existe a letra no nome
+			    for (int i = 0; i < nome.length(); i++) {
+			        char caractere = nome.charAt(i);
 
-		    for (int i = 0; i < nome.length(); i++) {
-		        char caractere = nome.charAt(i);
-
-		        if (caractere == 'a'){
-		            System.out.println(" Existe a letra A no nome mencionado.");
-		            
-		        }else
-					
-				 if(caractere == 'a') {
-		            System.out.println(" Não existe a letra  A no nome mencionado.");
-		        }
-		    }
+			        if (caractere == 'a'){
+			            System.out.println("Sim existe a letra 'a' no nome " + nome);
+			            
+			        }else
+						
+					 if(caractere == 'a') {
+			            System.out.println("Não existe a letra  'a' no nome " + nome);
+			        }
+			    }
+			    
+			 
+			 
+			 String letters = "";
+			 int countletters = letters.split("a").length;
+			 System.out.println("A quantidade de letra 'a' no nome " + nome +" é: " + countletters);
+			 
+			 // letra para identificar na String
+			//char letra = 'a';
+			
+			 //System.out.println(letra);  
+			 
+		 	
+		 
+		 
 			    
 		    //verificando a letra com o nome 
-		    int n = nomeLetra("fabio", 'a');
+		    int n = nomeLetra(nome, 'a');
 			
-			System.out.println(" Existe um total de " + n + " letra A no nome");
+			System.out.println("Existe um total de " + n + " letra A no nome " + nome);
 			
 		}// contador
 		private static int nomeLetra(String s, char c) {
