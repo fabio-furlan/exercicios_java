bf06a19fda8e10c49af22889ee8c388099132dbb
Fabio Furlan <furlan.fabio@hotmail.com>
2022-11-23 22:45:17 -0300

    Exercicios V2

--- Matriz/src/br/com/matriz/LetraX.java
+++ Matriz/src/br/com/matriz/LetraX.java
@@ -4,11 +4,11 @@ public class LetraX {
 		
 public static void main(String[] args) {
 		
-		int [][] basequadrado = new int[10][10];
+		int [][] baseX = new int[10][10];
 		
-		for (int linha = 0; linha < basequadrado.length; linha++) {
+		for (int linha = 0; linha < baseX.length; linha++) {
 			
-			for (int coluna = 0; coluna < basequadrado.length; coluna ++) {
+			for (int coluna = 0; coluna < baseX.length; coluna ++) {
 				
 				if (coluna > 0 && coluna < 9 && linha < 9 && linha >0 || linha > 10  && linha < 10 && coluna > 2 && coluna < 7) {
 					
